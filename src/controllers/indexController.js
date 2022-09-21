ebfb61354ece4c4b3fdb3b28181f9b6deec8e359
martin-pedraza <pedraza.martindiego@gmail.com>
2022-09-21 00:21:26 -0300

    details and list added

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,12 +1,32 @@
-const pokemonList = require('./../data/pokemon.json');
+const pokemonList = require('../data/pokemon.json');
 
 const indexController = {
     home: (req, res) => {
         res.render('index');
     },
     list: (req, res) => {
-        var pokemonType = req.params.id;
-        res.render('pokemon', {pokemonList: pokemonList, pokemonType: pokemonType});
+        let type = (req.params.type).toUpperCase();
+        let pokemonType=[];
+
+        for (const p of pokemonList) {
+            for (let j=0; j<p.type.length; j++) {
+                if (p.type[j] == type) {
+                    pokemonType.push(p);
+                }
+            }
+        }
+
+        res.render('pokemon', {pokemonType});
+    },
+    detail: (req, res) => {
+        let pokemonDetail;
+        for (const p of pokemonList) {
+            if(p.name == req.params.name){
+                pokemonDetail = p;
+            }
+        }
+
+        res.render('details', {pokemonDetail});
     }
 }
 
--- src/routes/indexRoute.js
+++ src/routes/indexRoute.js
@@ -4,6 +4,7 @@ const indexController = require('./../controllers/indexController');
 
 /* GET home page. */
 router.get('/', indexController.home);
-router.get('/types/:id', indexController.list);
+router.get('/types/:type', indexController.list);
+router.get('/details/:name', indexController.detail);
 
 module.exports = router;
