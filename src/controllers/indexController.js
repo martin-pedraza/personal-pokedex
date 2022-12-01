5455b5457336182b2ecfa230bd530d5a43265bd7
martin-pedraza <pedraza.martindiego@gmail.com>
2022-12-01 13:30:24 -0300

    format

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,33 +1,33 @@
 const pokemonList = require('../data/pokemon.json');
 
 const indexController = {
-    home: (req, res) => {
-        res.render('index');
-    },
-    list: (req, res) => {
-        let type = (req.params.type).toUpperCase();
-        let pokemonType=[];
+	home: (req, res) => {
+		res.render('index');
+	},
+	list: (req, res) => {
+		let type = req.params.type.toUpperCase();
+		let pokemonType = [];
 
-        for (const p of pokemonList) {
-            for (let j=0; j<p.type.length; j++) {
-                if (p.type[j] == type) {
-                    pokemonType.push(p);
-                }
-            }
-        }
+		for (const p of pokemonList) {
+			for (let j = 0; j < p.type.length; j++) {
+				if (p.type[j] == type) {
+					pokemonType.push(p);
+				}
+			}
+		}
 
-        res.render('pokemon', {pokemonType});
-    },
-    detail: (req, res) => {
-        let pokemonDetail;
-        for (const p of pokemonList) {
-            if(p.name == req.params.name){
-                pokemonDetail = p;
-            }
-        }
+		res.render('pokemon', { pokemonType });
+	},
+	detail: (req, res) => {
+		let pokemonDetail;
+		for (const p of pokemonList) {
+			if (p.name == req.params.name) {
+				pokemonDetail = p;
+			}
+		}
 
-        res.render('details', {pokemonDetail});
-    }
-}
+		res.render('details', { pokemonDetail });
+	}
+};
 
 module.exports = indexController;
